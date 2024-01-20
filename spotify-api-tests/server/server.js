20744032f06280533bdd26055416fac4f062a515
christina-mifsud <christina.mifsud@gmail.com>
2024-01-20 00:47:33 +0000

    Get Playlist endpoint

--- spotify-api-tests/server/server.js
+++ spotify-api-tests/server/server.js
@@ -32,10 +32,50 @@ async function getToken() {
 }
 
 // GET Artist endpoint
-// make API call & use the Get Artist endpoint to request info about artist
-async function getTrackInfo(access_token) {
+// // make API call & use the Get Artist endpoint to request info about artist
+// async function getTrackInfo(access_token) {
+//   const response = await fetch(
+//     "https://api.spotify.com/v1/tracks/22VHOlVYBqytsrAqV8yXBK",
+//     {
+//       // API call must include access token we just got above
+//       method: "GET",
+//       headers: { Authorization: "Bearer " + access_token },
+//     }
+//   );
+//   return await response.json();
+// }
+// // if we have a token, then run the getTrackInfo function to get info about artist in the console.log
+// getToken().then((response) => {
+//   getTrackInfo(response.access_token).then((profile) => {
+//     console.log(profile);
+//   });
+// });
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Get Playlist endpoint -> why is this coming up as an object not like this:
+// {
+//   "tracks": {
+//     "items": [
+//       {
+//         "added_at": "2024-01-18T20:08:53Z",
+//         "added_by": {
+//           "id": "31tulwjq2gsomhq5h5xdon2oopnu"
+//         }
+//       },
+//       {
+//         "added_at": "2024-01-09T22:05:02Z",
+//         "added_by": {
+//           "id": "31tulwjq2gsomhq5h5xdon2oopnu"
+//         }
+//       },}
+//     ]
+// }}
+// make API call & use the Get Playlist endpoint to get playlist owned by user
+async function getPlaylist(access_token) {
   const response = await fetch(
-    "https://api.spotify.com/v1/tracks/22VHOlVYBqytsrAqV8yXBK",
+    // "https://api.spotify.com/v1/playlists/2RoQkVgIhgQmCZadrMoDLd", // whole playlist
+    "https://api.spotify.com/v1/playlists/2RoQkVgIhgQmCZadrMoDLd?fields=tracks.items%28added_at%2Cadded_by.id%29",
     {
       // API call must include access token we just got above
       method: "GET",
@@ -44,9 +84,9 @@ async function getTrackInfo(access_token) {
   );
   return await response.json();
 }
-// if we have a token, then run the getTrackInfo function to get info about artist in the console.log
+// if we have a token, then run the getPlaylist function to get playlist info in the console.log
 getToken().then((response) => {
-  getTrackInfo(response.access_token).then((profile) => {
+  getPlaylist(response.access_token).then((profile) => {
     console.log(profile);
   });
 });
