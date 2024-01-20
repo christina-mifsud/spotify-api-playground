372daa16829f01271856ada96672b0c065e0e4e6
christina-mifsud <christina.mifsud@gmail.com>
2024-01-20 00:46:12 +0000

    fetch token & Get Artist endpoint

---
+++ spotify-api-tests/server/server.js
@@ -0,0 +1,52 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Client Credentials oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+ */
+
+const client_id = "";
+const client_secret = "";
+
+// fetches token (which expires after 1hr)
+async function getToken() {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    // POST request to the token endpoint URI
+    method: "POST",
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+    // Content-Type header set to the application/x-www-form-urlencoded value
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        // Added a HTTP body containing the Client ID and Client Secret
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+  });
+  // returns access token valid for 1hr
+  return await response.json();
+}
+
+// GET Artist endpoint
+// make API call & use the Get Artist endpoint to request info about artist
+async function getTrackInfo(access_token) {
+  const response = await fetch(
+    "https://api.spotify.com/v1/tracks/22VHOlVYBqytsrAqV8yXBK",
+    {
+      // API call must include access token we just got above
+      method: "GET",
+      headers: { Authorization: "Bearer " + access_token },
+    }
+  );
+  return await response.json();
+}
+// if we have a token, then run the getTrackInfo function to get info about artist in the console.log
+getToken().then((response) => {
+  getTrackInfo(response.access_token).then((profile) => {
+    console.log(profile);
+  });
+});
